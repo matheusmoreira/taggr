e0ad2d918b96e80c4fa7d0c773b29a25ecefcb04
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2023-10-02 04:31:15 -0300

    cli: fix misaligned colon in hash/tag map
    Huh. Must have not noticed it before.

--- taggr/cli.py
+++ taggr/cli.py
@@ -39,7 +39,7 @@ hash_function.hashlib_to_tag = {
     'sha3_224':   'SHA.3.224',
     'sha3_256':   'SHA.3.256',
     'sha3_384':   'SHA.3.384',
-    'sha3_512'  : 'SHA.3.512',
+    'sha3_512':   'SHA.3.512',
 }
 
 hash_function.choices = hashlib.algorithms_available.intersection(set(hash_function.hashlib_to_tag))
